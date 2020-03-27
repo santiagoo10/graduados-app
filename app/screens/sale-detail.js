b448c8cf671fe2f7125798be1784f8839e0b3618
santiago bolzico <santiago.bolzico@mercadolibre.com>
2020-03-27 19:30:42 -0300

    added screens

---
+++ app/screens/sale-detail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SaleDetail() {
+  return (
+    <View>
+      <Text>Detalle del Beneficio!</Text>
+    </View>
+  );
+}
---
+++ app/screens/sale-map.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SaleMap() {
+  return (
+    <View>
+      <Text>Mapa con los beneficios!</Text>
+    </View>
+  );
+}
