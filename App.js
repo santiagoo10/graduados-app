56611b782ac4352fc2650bbd7021b6560201ac7e
santiago bolzico <santiago.bolzico@mercadolibre.com>
2020-03-27 18:41:17 -0300

    added ui librery

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Graduados app!</Text>
+      <Text>Graduados App!</Text>
+      <Button
+        icon={<Icon name="arrow-right" size={15} color="white" />}
+        iconRight
+        title="Button with right icon"
+      />
     </View>
   );
 }
