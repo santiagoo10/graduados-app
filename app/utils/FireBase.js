246153088aa54599dfc98bc70a19ff16bb8b7e62
santiago bolzico <santiago.bolzico@mercadolibre.com>
2020-03-28 11:35:53 -0300

    added firebase librery

---
+++ app/utils/FireBase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzwJuySBcbP3NfGoxkE2jgKasOUmH6SxY",
+  authDomain: "graduados-a7240.firebaseapp.com",
+  databaseURL: "https://graduados-a7240.firebaseio.com",
+  projectId: "graduados-a7240",
+  storageBucket: "graduados-a7240.appspot.com",
+  messagingSenderId: "714619466530",
+  appId: "1:714619466530:web:34cc5fb2b5dd61c77a6b7b"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
