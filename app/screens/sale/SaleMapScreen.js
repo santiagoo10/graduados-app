7d005260d6a42d9df4c929a793e2eb2ca1c31177
santiago bolzico <santiago.bolzico@mercadolibre.com>
2020-04-10 13:12:02 -0300

    added map in mapscreen

--- app/screens/sale/SaleMapScreen.js
+++ app/screens/sale/SaleMapScreen.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ScrollView, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+
+const screenWidth = Dimensions.get("window").height;
 
 export default function SaleMap() {
+  //TODO: falta consultar todos los beneficios y hacer un marker por cada uno.
+
+  const location = {
+    latitude: -31.653788758943733,
+    latitudeDelta: 0.0013262862751375337,
+    longitude: -60.70879757890222,
+    longitudeDelta: 0.0010000000000331966,
+  };
+
   return (
-    <View>
-      <Text>Mapa con los beneficios!</Text>
-    </View>
+    <ScrollView>
+      <MapView
+        style={{ width: "100%", height: screenWidth }}
+        initialRegion={location}
+        //onPress={openAppMap}
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+        />
+      </MapView>
+    </ScrollView>
   );
 }
